9a6adb03f288d4b552712dd996f776610028ba32
Mircea Tanasa <tam@ad.rms.intern>
2018-09-19 13:41:30 +0300

    add min max problem

--- second/1-maxim-minim/main.js
+++ second/1-maxim-minim/main.js
@@ -1,14 +1,25 @@
 let formSubmit = function(event) {
     event.preventDefault();
     let input = event.target[0].value,
-        invertedNumber = 0;
+        inputVector = input.split(','),
+        min,
+        max = 0;
 
-    while (input >= 1) {
-        invertedNumber = invertedNumber * 10 + (input % 10);
-        input = Math.floor(input / 10);
+        console.log(`Input vector is ${inputVector}`)
+
+    for (let index = 0; index < inputVector.length; index++) {
+        if (+inputVector[index] > max) {
+            max = inputVector[index];
+        }
+
+        if (!min || +inputVector[index] < min) {
+            min = inputVector[index];
+        }
     }
 
-    console.log('Inverted number: ', invertedNumber);
+    console.log(`Min is ${min} and max is ${max}`);
+
+
 };
 
 window.addEventListener('submit', formSubmit);
