352f4b36a2f2a1da11e792257fea29150f7666a7
Mircea Tanasa <tam@ad.rms.intern>
2018-09-19 09:50:11 +0300

    rename palindrom value

--- verificare-palindrom/main.js
+++ verificare-palindrom/main.js
@@ -1,33 +1,21 @@
 let formSubmit = function(event) {
     event.preventDefault();
-    let value = event.target[0].value;
+    let input = event.target[0].value;
 
-    console.log('submit ', value);
-    
-    if (!value || !isNaN(value)) {
-        console.log('invalid value');
-        return;
-    }
-
-    let valueArray = value.split(''),
-        valueArrayLength = valueArray.length - 1,
+    let inputArray = '' + input.split(''),
+        inputArrayLength = inputArray.length - 1,
         isPalindrom = true;
 
-    console.log('value array ', valueArray);
-    console.log('value array length ', valueArrayLength);
-
-    for (let index = 0; index < valueArrayLength / 2; index++) {
-        console.log('index', index);
-
-        if (valueArray[index] !== valueArray[valueArrayLength - index] ) {
-            console.log('The value isn\'t palindrom');
+    for (let index = 0; index < inputArrayLength / 2; index++) {
+        if (inputArray[index] !== inputArray[inputArrayLength - index] ) {
+            console.log('The input isn\'t palindrom');
             isPalindrom = false;
             break;
         }
     }
 
     if (isPalindrom) {
-        console.log('The value is palindrom');
+        console.log('The input is palindrom');
     }
 };
 
