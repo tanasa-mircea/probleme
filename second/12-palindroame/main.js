883e89b26fcfd9e7ca59db9b8558dc6220761a70
Mircea <tanasamircea@yahoo.com>
2018-09-20 07:07:58 +0300

    add palindroame progress

---
+++ second/12-palindroame/main.js
@@ -0,0 +1,41 @@
+let formSubmit = function(event) {
+    event.preventDefault();
+    let input = event.target[0].value,
+        inputVector = input.split(','),
+        palindroms = [];
+
+    for (let index = 0; index < inputVector.length - 1; index++) {
+        let subvectorLength = inputVector.length - index;
+            numberOfPossibleStartPositions = index + 1;
+        
+        for (let j = 0; j < numberOfPossibleStartPositions; j++) {
+            let subvector = inputVector.slice(j, subvectorLength);
+            console.log('inputVector ', inputVector);
+            console.log('Subvector ', subvector);
+
+            if (isPalindrom(subvector)) {
+                palindroms.push(subvector);
+            }
+        }   
+    }
+
+    console.log('Palindroms are ', palindroms)
+
+    
+};
+
+function isPalindrom(vector) {
+    let isPalindrom = true;
+
+    for (let index = 0; index < vector.length / 2; index++) {
+
+        if (vector[index] !== vector[vector.length - 1 - index]) {
+            isPalindrom = false;
+            break;
+        }
+    }
+
+    return isPalindrom;
+}
+
+window.addEventListener('submit', formSubmit);
