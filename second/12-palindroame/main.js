142b4f5afbde0a9d9f6762fc69d6a4550adebca2
Mircea Tanasa <tam@ad.rms.intern>
2018-09-20 08:10:22 +0300

    update palindroame

--- second/12-palindroame/main.js
+++ second/12-palindroame/main.js
@@ -9,10 +9,8 @@ let formSubmit = function(event) {
             numberOfPossibleStartPositions = index + 1;
         
         for (let j = 0; j < numberOfPossibleStartPositions; j++) {
-            let subvector = inputVector.slice(j, subvectorLength);
-            console.log('inputVector ', inputVector);
-            console.log('Subvector ', subvector);
-
+            let subvector = inputVector.slice(j, subvectorLength + j);
+            
             if (isPalindrom(subvector)) {
                 palindroms.push(subvector);
             }
@@ -29,7 +27,7 @@ function isPalindrom(vector) {
 
     for (let index = 0; index < vector.length / 2; index++) {
 
-        if (vector[index] !== vector[vector.length - 1 - index]) {
+        if (vector.length < 2 || vector[index] !== vector[vector.length - 1 - index]) {
             isPalindrom = false;
             break;
         }
