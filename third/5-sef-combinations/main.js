ce014d715f41ef2c93a20af596ab741c4e8549a4
Mircea Tanasa <tam@ad.rms.intern>
2018-09-21 16:10:11 +0300

    fix sef-combinations

--- third/5-sef-combinations/main.js
+++ third/5-sef-combinations/main.js
@@ -4,7 +4,8 @@ let formSubmit = function(event) {
         numberOfZeros = convertToBinary(code);
         count = 0;
     
-    console.log(numberOfZeros);
+    console.log('code ', code.toString(2));
+    console.log('Nr zero ', numberOfZeros);
 
     for (let index = 1; index <= numberOfZeros; index++) {
         count += factorial(numberOfZeros) / (factorial(index) * factorial(numberOfZeros - index));
@@ -24,7 +25,7 @@ let formSubmit = function(event) {
         let isDone = false,
             result = [],
             index = 0,
-            numberOfZero = 16;
+            numberOfZero = 0;
 
         result.length = 16;
 
@@ -39,12 +40,18 @@ let formSubmit = function(event) {
 
         while (!isDone) {
             result[result.length - 1 - index] = Math.floor(codeToConvert % 2)
+
+            if (result[result.length - 1 - index] === 0) {
+                numberOfZero++;
+            }
+
             isDone = (Math.floor(codeToConvert % 2) === 1 && Math.floor(codeToConvert / 2) === 0);
             codeToConvert = Math.floor(codeToConvert / 2);
             index++;
+
         }
 
-        return numberOfZero - index;
+        return numberOfZero;
     }
 
     console.log('count ', count);
