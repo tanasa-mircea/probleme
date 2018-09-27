200d26c4a198c6f9de4967d434844c10031253a0
Mircea Tanasa <tam@ad.rms.intern>
2018-09-27 11:12:42 +0300

    add element pulsatoriu update

--- js/element-pulsatoriu/main.js
+++ js/element-pulsatoriu/main.js
@@ -5,27 +5,30 @@ function contentLoadedHandler() {
     let element = document.querySelector('.pulsating-element--js'),
         animationConfig = {
             target: element,
+            currentPosition: element.offsetWidth,
             minSize: 100,
             maxSize: 200,
             time: 1000,
-            step: 2.2
+            step: 1
         }
 
     window.requestAnimationFrame(init.bind(animationConfig));
 };
 
 function init() {
-    let currentDiameter = this.target.offsetWidth;
+    let currentDiameter = this.currentPosition;
     
     if (isForward) {
         this.target.style.width = currentDiameter + this.step + "px";
         this.target.style.height = currentDiameter + this.step + "px";
+        this.currentPosition += this.step;
         isForward = currentDiameter + this.step < this.maxSize; 
     }
 
     if (!isForward) {
         this.target.style.width = currentDiameter - this.step + "px";
         this.target.style.height = currentDiameter - this.step + "px";
+        this.currentPosition -= this.step;
         isForward = currentDiameter - this.step < this.minSize; 
     }
 
