4291528ff35f22ccf046b2e4b15c08757860fee9
Mircea Tanasa <tam@ad.rms.intern>
2018-09-27 09:36:36 +0300

    update coliziune

--- js/coliziune/main.js
+++ js/coliziune/main.js
@@ -1,3 +1,29 @@
+function initMouseHandlers(elements) {
+    var mouseDown = false;
+
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('mousedown', function circleElementHandler(event) {
+            mouseDown = this;
+        })       
+    }
+
+    window.addEventListener('mousemove', mouseMoveHandler);
+    window.addEventListener('mouseup', mouseUpHandler);
+
+    function mouseMoveHandler() {
+        if (mouseDown) {
+            mouseDown.style.left = event.x - mouseDown.offsetParent.offsetLeft - mouseDown.offsetWidth / 2 + 'px';
+            mouseDown.style.top = event.y - mouseDown.offsetParent.offsetTop - mouseDown.offsetHeight / 2 + 'px';
+        }
+    }
+    
+    function mouseUpHandler() {
+        mouseDown = null;
+        window.removeEventListener('mousemove', mouseMoveHandler);
+        window.removeEventListener('mouseup', mouseUpHandler);
+    }
+}
+
 function contentLoadedHandler() {
     var elements = document.querySelectorAll('.patrat'),
         circleElements = document.querySelectorAll('.circle')
@@ -8,15 +34,41 @@ function contentLoadedHandler() {
             secondCircleElement: circleElements[1]
         }
     
+    initMouseHandlers([...elements, ...circleElements]);
+
     window.requestAnimationFrame(init.bind(animationConfig));
-    init.call(animationConfig);
+    // init.call(animationConfig);
 };
 
 function init() {
     isColiding(this.firstElement, this.secondElement);
-    isColiding(this.firstCircleElement, this.secondCircleElement);
+    isColidingCircle(this.firstCircleElement, this.secondCircleElement);
 
     window.requestAnimationFrame(init.bind(this));
+}
+
+function isColidingCircle(firstElement, secondElement) {
+    let colision = false,
+        firstElementData = {
+            center: [firstElement.offsetLeft + firstElement.offsetWidth / 2, firstElement.offsetTop + firstElement.offsetHeight / 2],
+            radius: firstElement.offsetWidth / 2
+        },
+        secondElementData = {
+            center: [secondElement.offsetLeft + secondElement.offsetWidth / 2, secondElement.offsetTop + secondElement.offsetHeight / 2],
+            radius: secondElement.offsetWidth / 2
+        },
+        distanceBetweenCenters = Math.pow(secondElementData.center[0] - firstElementData.center[0], 2) + Math.pow(secondElementData.center[1] - firstElementData.center[1], 2),
+        sumOfRadius = Math.pow(firstElementData.radius + secondElementData.radius, 2);
+
+    if (distanceBetweenCenters < sumOfRadius) {
+        colision = true;
+    }
+
+    if (colision) {
+        secondElement.style.backgroundColor = "#dd0"
+    } else {
+        secondElement.style.backgroundColor = null;
+    } 
 }
 
 function isColiding(firstElement, secondElement) {
@@ -35,19 +87,19 @@ function isColiding(firstElement, secondElement) {
             bottom: secondElement.offsetTop + secondElement.offsetHeight
         };
 
-        if (secondElementData.left < firstElementData.right && secondElementData.right > firstElementData.left) {
-            colisionX = true;
-        }
-        
-        if (secondElementData.bottom > firstElementData.top && secondElementData.top < firstElementData.bottom) {
-            colisionY = true
-        }
+    if (secondElementData.left < firstElementData.right && secondElementData.right > firstElementData.left) {
+        colisionX = true;
+    }
+    
+    if (secondElementData.bottom > firstElementData.top && secondElementData.top < firstElementData.bottom) {
+        colisionY = true
+    }
 
-        if (colisionX && colisionY) {
-            secondElement.style.backgroundColor = "#dd0"
-        } else {
-            secondElement.style.backgroundColor = null;
-        }
+    if (colisionX && colisionY) {
+        secondElement.style.backgroundColor = "#dd0"
+    } else {
+        secondElement.style.backgroundColor = null;
+    }
 
     return colisionX && colisionY;
 }
