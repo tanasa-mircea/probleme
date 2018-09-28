008aa76ff1e7a1871916cff9fa57c6a2fc6f70cd
Mircea Tanasa <tam@ad.rms.intern>
2018-09-28 14:01:07 +0300

    add item list

---
+++ js/item-list/main.js
@@ -0,0 +1,46 @@
+var counter = 1;
+
+function contentLoadedHandler() {
+    let list = document.querySelector('.list'),
+        listControl = document.querySelector('.list-control');
+        // newListItem = createListElement(listControl); 
+    
+    list.addEventListener('click', function(event) {
+        let newListItem = createListElement(listControl); 
+
+        if (!event.target.matches('button')) {
+            return;
+        }
+
+        let action = event.target.dataset.action,
+            currentLi = event.target.parentNode.parentNode;
+
+        if (action === 'add') {
+            list.insertBefore(newListItem.cloneNode(true), currentLi.nextSibling);
+        }
+
+        if (action === 'remove') {
+            list.removeChild(currentLi);
+        }
+    })
+};
+
+function createListElement(liControl) {
+    let li = document.createElement('li'),
+        liInfo = document.createElement('div'),
+        liTitle = document.createElement('h2'),
+        liSubtitle = document.createElement('h3');
+
+    liTitle.innerText = 'item ' + counter;
+    liSubtitle.innerText = 'desc ' + counter;
+
+    liInfo.appendChild(liTitle);
+    liInfo.appendChild(liSubtitle);
+    li.appendChild(liInfo);
+    li.appendChild(liControl.cloneNode(true));
+
+    counter++;
+    return li;
+}
+  
+document.addEventListener("DOMContentLoaded", contentLoadedHandler);
