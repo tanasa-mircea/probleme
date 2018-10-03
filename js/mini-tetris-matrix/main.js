8fd9d653dab8129534f0d4029ee43d39f4107c00
Mircea Tanasa <tam@ad.rms.intern>
2018-10-03 11:16:37 +0300

    update mini-tetris with matrix

--- js/mini-tetris-matrix/main.js
+++ js/mini-tetris-matrix/main.js
@@ -2,7 +2,7 @@ var config = {
         columns: 5,
         figureSide: 40,
         rows: 15,
-        speed: 3 / 100
+        speed: 5 / 100
     },
     currentAnimation,
     currentPosition = {
@@ -10,8 +10,10 @@ var config = {
         y: 0,
         roundedY: 0
     },
+    previousPosition, nextPosition,
     playground,
     playgroundModel = [],
+    playgroundNodes = [],
     isPaused = false,
     shouldReset = false,
     score = 0,
@@ -36,11 +38,15 @@ function initPlayground() {
     playground.style.width = config.columns * config.figureSide + 'px';
     
     for (let i = 0; i < config.rows; i++) {
+        playgroundNodes[i] = [];
+        playgroundModel[i] = [];
+
         for (let j = 0; j < config.columns; j++) {
             let clone = matrixElement.cloneNode();
             clone.id = `m-${i}-${j}`
             playground.appendChild(clone)
-            playgroundModel[i] = [];
+            playgroundNodes[i][j] = clone;
+            playgroundModel[i][j] = 0;
         }
     }
 }
@@ -59,24 +65,70 @@ function frameHandler() {
     if (!currentPosition) {
         currentPosition = {
             x: Math.floor(config.columns / 2),
-            y: 0
+            y: 0,
+            roundedY: 0
         }
     }
     
     currentPosition.y += config.speed;
+    nextPosition = playgroundModel[currentPosition.roundedY + 1] && playgroundModel[currentPosition.roundedY + 1][currentPosition.x];
 
-    if (currentPosition.roundedY < Math.floor(currentPosition.y)) {
-        let currentNode = document.getElementById(`m-${Math.floor(currentPosition.y)}-${Math.floor(currentPosition.x)}`),
-            prevNode = document.getElementById(`m-${Math.floor(currentPosition.y - 1)}-${Math.floor(currentPosition.x)}`);
-
-        prevNode.classList.remove('active');
-        currentNode.classList.add('active');
-        currentPosition.roundedY = Math.floor(currentPosition.y);
+    if (currentPosition.roundedY >= config.rows - 1 || nextPosition) {
+        previousPosition = null;
+        currentPosition = null;
     }
 
+    if (currentPosition && currentPosition.roundedY < Math.floor(currentPosition.y)) {
+        currentPosition.roundedY = Math.floor(currentPosition.y);
+
+        playgroundModel[currentPosition.roundedY][currentPosition.x] = 1;
+
+        if (previousPosition && playgroundModel[previousPosition.roundedY - 1] ) {
+            playgroundModel[previousPosition.roundedY][previousPosition.x] = 0;
+        }
+        
+        checkIfLineCompleted();
+        repaint();
+        previousPosition = Object.assign({}, currentPosition);
+    }
+    
     window.requestAnimationFrame(frameHandler);
 }
 
+function repaint() {
+    for (let i = 0; i < config.rows; i++) {
+        for (let j = 0; j < config.columns; j++) {
+            let element = playgroundNodes[i][j];
+
+            if (playgroundModel[i][j]) {
+                element.classList.add('active');
+            } else {
+                element.classList.remove('active');
+            }
+        }        
+    }
+}
+
+function checkIfLineCompleted() {
+    let isLineComplete = true;
+
+    for (let i = 0; i < config.columns; i++) {
+        if (!playgroundModel[config.rows - 1][i]) {
+            isLineComplete = false;
+        }
+    }
+
+    if (isLineComplete) {
+        for (let i = config.rows - 1; i >= 0 ; i--) {
+            for (let j = 0; j < config.columns; j++) {
+                playgroundModel[i][j] = playgroundModel[i - 1] ? playgroundModel[i - 1][j] : 0;
+            }        
+        }
+
+        score++;
+        scoreElement.innerHTML = score;
+    }
+}
 
 function pauseGame() {
     if (!currentAnimation) {
@@ -96,15 +148,21 @@ function keydownHandler(event) {
     }
 
     if (event.keyCode === 39) {
-        let prevNode = document.getElementById(`m-${Math.floor(currentPosition.y)}-${Math.floor(currentPosition.x)}`);
-        prevNode.classList.remove('active');
-        currentPosition.x++;
+        let itemOnRight = playgroundModel[currentPosition.roundedY][currentPosition.x + 1];
+
+        if (!itemOnRight) {
+            currentPosition.x = Math.min(++currentPosition.x, config.columns - 1);
+            repaint();
+        }
     }
     
     if (event.keyCode === 37) {
-        let prevNode = document.getElementById(`m-${Math.floor(currentPosition.y)}-${Math.floor(currentPosition.x)}`);
-        prevNode.classList.remove('active');
-        currentPosition.x--;
+        let itemOnLeft = playgroundModel[currentPosition.roundedY][currentPosition.x - 1];
+
+        if (!itemOnLeft) {
+            currentPosition.x = Math.max(--currentPosition.x, 0);
+            repaint();
+        }
     }
 
     if (event.keyCode === 40) {
@@ -127,8 +185,6 @@ function inputChangeHandler(event) {
 
 function clickHandler(event) {
     if (event.target.matches('.game-control')) {
-        console.log(event.target.attrs)
-
         let action = event.target.dataset.action;
 
         if (action === 'reset') {
