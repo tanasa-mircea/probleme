7423c33cef32e11856362b4803d785ebf6cbd8ea
Mircea Tanasa <tam@ad.rms.intern>
2018-10-10 10:11:19 +0300

    update fractal

--- algorithms/fifth/8-fractali-patrate/main.js
+++ algorithms/fifth/8-fractali-patrate/main.js
@@ -2,7 +2,7 @@ let wrapperElement, newNumber, squareSide;
 let squareElement = document.createElement('div');
 squareElement.classList.add('square');
 
-let iterationsNumber = 3,
+let iterationsNumber = 4,
     initSquareSize = 400;
 
 function contentLoadedHandler() {
@@ -15,9 +15,7 @@ function generateFractal(number) {
 
     newNumber = number * 4;
     if (newNumber <= Math.pow(4, iterationsNumber)) {
-        setTimeout(() => {
-            return generateFractal(newNumber)
-        }, 300)
+        return generateFractal(newNumber);
     } else {
         return false;
     }
@@ -36,19 +34,24 @@ function paintSquares(count, number, index, rowCount) {
     squareSide = (initSquareSize / Math.sqrt(number)) * 100 / initSquareSize;
     squareElement.style.height = initSquareSize / Math.sqrt(number) + 'px';
     squareElement.style.width = initSquareSize / Math.sqrt(number) + 'px';
+    squareElement.style.borderColor = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
 
     if (number === 1) {
         squareElement.style.left = '50%';
         squareElement.style.top = '50%';
         squareElement.style.borderWidth = '10px';
-        squareElement.style.transform = 'translate(-50%, -50%)';
     } else {
         squareElement.style.borderWidth = `${10 - Math.sqrt(number)}px`;
         squareElement.style.left = squareSide / 2 +  index * squareSide  + '%';
         squareElement.style.top = squareSide / 2 + rowCount  * squareSide  +  '%';
     }
+    let clone = squareElement.cloneNode();
 
-    wrapperElement.appendChild(squareElement.cloneNode());
+    wrapperElement.appendChild(clone);
+
+    setTimeout(() => {
+        clone.style.transform = 'translate(-50%, -50%) scale(1)';
+    }, 500 + Math.random() * 1000);
 
     return paintSquares(++count, number, ++index, rowCount);
 }
